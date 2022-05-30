29cb89677caab2121cce5a557426ae6342444d4f
Personal <personal@Camerons-MacBook-Pro.local>
2022-05-29 21:26:10 -0400

    created navigation component and added routing

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -1,38 +1,25 @@
-import Directory from "./components/Directory/Directory";
+import { Routes, Route } from 'react-router-dom'
+
+import Home from "./routes/Home/Home";
+import Navigation from './routes/Navigation/Navigation';
+
+const Shop = () => {
+  return (
+    <div>
+      This is the shop page
+    </div>
+  )
+}
 
 const App = () => {
-
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
-
   return (
-    <Directory categories={categories}/>
-  );
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App;
